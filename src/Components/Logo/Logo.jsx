5f41b50203693c2a7171659085e747d8263aafb8
chen7647 <chen7647@umn.edu>
2025-01-29 23:11:05 -0600

    added nav and logo and stylings

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-
+import Tilt from 'react-parallax-tilt';
+import logo from './logo.jpg'
+import './Logo.css';
 export class Logo extends Component {
   render() {
     return (
-      <div>
-        <p>this will be my Logo</p>
+      <div className="ma4 mt0">
+        <Tilt className="Tilt br4 shadow-2">
+          {/* <h1>React Parallax Tilt 👀</h1>
+          <p>this will be my Logo (with Tilt animation)</p> */}
+            <div className="Tilt-inner logo-img"><img src={logo} className="Tilt-inner logo-img img"alt="logo"></img></div>
+        </Tilt>
       </div>
     )
   }
