f2dcf941d6f0386ac96488964f8a5d41e4ab754a
chen7647 <chen7647@umn.edu>
2025-02-08 16:17:17 -0600

    correct logo style

--- src/Components/Logo/Logo.jsx
+++ src/Components/Logo/Logo.jsx
@@ -6,10 +6,10 @@ export class Logo extends Component {
   render() {
     return (
       <div className="ma4 mt0">
-        <Tilt className="Tilt br4">
-          {/* <h1>React Parallax Tilt 👀</h1>
-          <p>this will be my Logo (with Tilt animation)</p> */}
-            <div className="Tilt-inner logo-img"><img src={logo} className="Tilt-inner logo-img img"alt="logo"></img></div>
+        <Tilt className="br4 Tilt">
+          <div >
+            <img src={logo} className="Tilt-inner logo-img" alt="logo" ></img>
+          </div>
         </Tilt>
       </div>
     )
